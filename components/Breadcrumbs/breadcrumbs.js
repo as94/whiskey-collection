a4ffd303c8cac901375090bbded95aaeadb54de1
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2023-05-23 21:42:40 +0600

    add breadcrumbs

---
+++ components/Breadcrumbs/breadcrumbs.js
@@ -0,0 +1,23 @@
+const getItems = items => {
+  let result = '';
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index];
+    result +=
+      index === items.length - 1
+        ? `<span class="item body-text-16">${item}</span>`
+        : `
+    <span class="item body-text-16">${item}</span>
+    <img class="chevron" src="icons/chevron-right.svg" />
+`;
+  }
+  return result;
+};
+
+const breadcrumbs = items => `
+<link rel="stylesheet" href="./components/Breadcrumbs/breadcrumbs.css" />
+<div class="breadcrumbs">
+    ${getItems(items)}
+</div>
+`;
+
+$('#breadcrumbs').html(breadcrumbs(['Home', 'Scotch Whiskey']));
