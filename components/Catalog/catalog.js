b338ffe1c7f760a9e43e873c1e4a802095200019
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2023-05-28 20:31:48 +0600

    extract catalog component

---
+++ components/Catalog/catalog.js
@@ -0,0 +1,164 @@
+const catalog = `
+<link rel="stylesheet" href="./components/Catalog/catalog.css" />
+<div class="catalog">
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/dark-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/aberlour.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/light-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/glenfiddich.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/dark-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/old_forester.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/light-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/jameson.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/dark-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/yamazaki.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/dark-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/aberlour.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/dark-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/yamazaki.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/dark-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/aberlour.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/dark-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/yamazaki.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/dark-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/aberlour.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <div class="whiskey-image">
+      <img
+        class="background-image"
+        src="images/product-card-backgrounds/dark-green.png"
+      />
+      <img class="foreground-image" src="images/whiskey/yamazaki.png" />
+    </div>
+    <div class="whiskey-name body-text-18">
+      Angel’s Envy Kentucky Straight Bourbon Whiskey
+    </div>
+    <div class="whiskey-characteristics body-text-18">70cl/40%</div>
+  </div>
+
+  <div class="card">
+    <img src="images/atmosphere-1.jpg" />
+  </div>
+</div>
+`;
+
+$('#catalog').html(catalog);
---
+++ components/CatalogFooter/catalogFooter.js
@@ -0,0 +1,8 @@
+const catalogFooter = `
+<link rel="stylesheet" href="./components/CatalogFooter/catalogFooter.css" />
+<div class="catalog-footer">
+  <div id="pagination" class="pagination"></div>
+</div>
+`;
+
+$('#catalogFooter').html(catalogFooter);
