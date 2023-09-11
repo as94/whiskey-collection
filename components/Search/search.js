c93c858a9041e10809f4b245a351a50274673315
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2023-09-12 00:54:17 +0600

    wip: better search filters

--- components/Search/search.js
+++ components/Search/search.js
@@ -2,8 +2,8 @@ import { registerBlockTitle } from '../BlockTitle/blockTitle.js';
 import { initializeWhiskey } from '../../services/loadWhiskey.js';
 import {
   getCountries,
-  getBrands,
-  getBudgetRanges,
+  getBrandsByCountry,
+  getBudgetsByBrand,
 } from '../../services/state.js';
 import { goToCatalogBySearchResults } from '../../services/urlSearchParams.js';
 
@@ -12,24 +12,6 @@ const generateCountryListItems = () => {
   const countries = getCountries();
   for (const country of countries) {
     result += `<li>${country}</li>`;
-  }
-  return result;
-};
-
-const generateBrandListItems = () => {
-  let result = '';
-  const brands = getBrands();
-  for (const brand of brands) {
-    result += `<li>${brand}</li>`;
-  }
-  return result;
-};
-
-const generateBudgetListItems = () => {
-  let result = '';
-  const budgetRanges = getBudgetRanges();
-  for (const budgetRange of budgetRanges) {
-    result += `<li>${budgetRange}</li>`;
   }
   return result;
 };
@@ -63,7 +45,7 @@ const search = () => {
           What brand?
         </label>
         <div class="dropdown-container" data-no-select>
-          <div class="selected-item brand">
+          <div class="selected-item brand disabled">
             <span id="selected-brand" class="body-medium">Any</span>
             <div class="dropdown-controls">
               <div class="clean"></div>
@@ -71,7 +53,6 @@ const search = () => {
             </div>
           </div>
           <ul class="dropdown-options brand">
-            ${generateBrandListItems()}
           </ul>
         </div>
       </div>
@@ -80,7 +61,7 @@ const search = () => {
           What budget?
         </label>
         <div class="dropdown-container" data-no-select>
-          <div class="selected-item budget">
+          <div class="selected-item budget disabled">
             <span id="selected-budget" class="body-medium">Any</span>
             <div class="dropdown-controls">
               <div class="clean"></div>
@@ -88,7 +69,6 @@ const search = () => {
             </div>
           </div>
           <ul class="dropdown-options budget">
-            ${generateBudgetListItems()}
           </ul>
         </div>
       </div>
@@ -114,28 +94,68 @@ if (element) {
 
 registerBlockTitle('search-block-title');
 
-const filters = ['country', 'brand', 'budget'];
 const root = '.filter-block .dropdown-container';
 
-filters.forEach(filter => {
+const fillBrands = () => {
+  const oldList = document.querySelector(`${root} .dropdown-options.brand`);
+  const parent = oldList.parentNode;
+
+  const newList = document.createElement('ul');
+  newList.classList.add('dropdown-options', 'brand');
+
+  const country = document.querySelector('#selected-country').textContent;
+  const brands = getBrandsByCountry(country);
+  for (const brand of brands) {
+    const li = document.createElement('li');
+    li.textContent = brand;
+    newList.appendChild(li);
+  }
+
+  parent.replaceChild(newList, oldList);
+};
+
+const fillBudgets = () => {
+  const oldList = document.querySelector(`${root} .dropdown-options.budget`);
+  const parent = oldList.parentNode;
+
+  const newList = document.createElement('ul');
+  newList.classList.add('dropdown-options', 'budget');
+
+  const brand = document.querySelector('#selected-brand').textContent;
+  const budgets = getBudgetsByBrand(brand);
+  for (const budget of budgets) {
+    const li = document.createElement('li');
+    li.textContent = budget;
+    newList.appendChild(li);
+  }
+
+  parent.replaceChild(newList, oldList);
+};
+
+const addEventListeners = filter => {
   const selectedFilter = document.querySelector(
     `${root} .selected-item.${filter}`
   );
-
-  selectedFilter.addEventListener('click', function () {
-    this.classList.toggle('active');
-    const dropdownOptions = document.querySelector(
-      `${root} .dropdown-options.${filter}`
-    );
-    dropdownOptions.classList.toggle('show');
-  });
 
   const cleanButton = selectedFilter.querySelector('.clean');
   const options = document.querySelectorAll(
     `${root} .dropdown-options.${filter} > li`
   );
   const selectedText = document.querySelector(`#selected-${filter}`);
-  cleanButton.addEventListener('click', function (event) {
+
+  const selectedFilterClickHandler = function () {
+    const isDisabled = this.classList.contains('disabled');
+    if (isDisabled) {
+      return;
+    }
+    this.classList.toggle('active');
+    const dropdownOptions = document.querySelector(
+      `${root} .dropdown-options.${filter}`
+    );
+    dropdownOptions.classList.toggle('show');
+  };
+
+  const cleanButtonClickHandler = function (event) {
     event.stopPropagation();
     options.forEach(option => option.classList.remove('selected'));
     document
@@ -145,23 +165,84 @@ filters.forEach(filter => {
       .querySelectorAll(`${root} .dropdown-options`)
       .forEach(option => option.classList.remove('show'));
     selectedText.textContent = 'Any';
-  });
+
+    if (filter === 'country') {
+      document.getElementById('selected-brand').textContent = 'Any';
+      const brandFilter = document.querySelector(
+        `${root} .selected-item.brand`
+      );
+      const cleanBrand = brandFilter.querySelector('.clean');
+      cleanBrand.click();
+      // brandFilter.classList.add('disabled');
+      // addEventListeners('brand');
+    }
+
+    if (filter === 'brand') {
+      const budgetFilter = document.querySelector(
+        `${root} .selected-item.budget`
+      );
+      const cleanBudget = budgetFilter.querySelector('.clean');
+      cleanBudget.click();
+      // budgetFilter.classList.add('disabled');
+      // addEventListeners('budget');
+    }
+  };
+
+  const optionClickHandler = function () {
+    const selectedOption = this.textContent;
+    selectedText.textContent = selectedOption;
+    options.forEach(opt => opt.classList.remove('selected'));
+    this.classList.add('selected');
+    document
+      .querySelectorAll(`${root} .dropdown-options`)
+      .forEach(option => option.classList.remove('show'));
+    document
+      .querySelectorAll(`${root} .selected-item`)
+      .forEach(item => item.classList.remove('active'));
+    if (filter === 'country') {
+      const brandFilter = document.querySelector(
+        `${root} .selected-item.brand`
+      );
+
+      if (!brandFilter.classList.contains('disabled')) {
+        addEventListeners('brand');
+      }
+
+      fillBrands();
+
+      brandFilter.classList.remove('disabled');
+      document.getElementById('selected-brand').textContent = 'Any';
+
+      addEventListeners('brand');
+    }
+
+    if (filter === 'brand') {
+      const budgetFilter = document.querySelector(
+        `${root} .selected-item.budget`
+      );
+
+      if (!budgetFilter.classList.contains('disabled')) {
+        addEventListeners('budget');
+      }
+
+      fillBudgets();
+
+      budgetFilter.classList.remove('disabled');
+      document.getElementById('selected-budget').textContent = 'Any';
+
+      addEventListeners('budget');
+    }
+  };
+
+  selectedFilter.addEventListener('click', selectedFilterClickHandler);
+  cleanButton.addEventListener('click', cleanButtonClickHandler);
 
   options.forEach(option => {
-    option.addEventListener('click', function () {
-      const selectedOption = this.textContent;
-      selectedText.textContent = selectedOption;
-      options.forEach(opt => opt.classList.remove('selected'));
-      this.classList.add('selected');
-      document
-        .querySelectorAll(`${root} .dropdown-options`)
-        .forEach(option => option.classList.remove('show'));
-      document
-        .querySelectorAll(`${root} .selected-item`)
-        .forEach(item => item.classList.remove('active'));
-    });
+    option.addEventListener('click', optionClickHandler);
   });
-});
+};
+
+addEventListeners('country');
 
 document.addEventListener('click', event => {
   const target = event.target;
--- services/state.js
+++ services/state.js
@@ -1,12 +1,23 @@
-import { groupBy, toDictionary } from './utils.js';
+import { groupBy, groupByWithSelect, toDictionary } from './utils.js';
 
 const emptyFilter = 'Any';
+
+const budgetRanges = [
+  '$0 - $30',
+  '$30 - $60',
+  '$60 - $90',
+  '$90 - $120',
+  '$120 - $150',
+  '$150 - $2000',
+];
 
 const state = {
   whiskey: [],
   whiskeyByCategory: {},
   whiskeyByName: {},
   whiskeyByBrand: {},
+  brandsByCountry: {},
+  budgetsByBrand: {},
   mainCategories: [],
   countries: [],
   brands: [],
@@ -18,6 +29,8 @@ export const setWhiskey = whiskey => {
   initWhiskeyByCategory(whiskey);
   initWhiskeyByName(whiskey);
   initWhiskeyByBrand(whiskey);
+  initBrandsByCountry(whiskey);
+  initBudgetsByBrand(whiskey);
   initCountries(whiskey);
   initBrands(whiskey);
   initBudgetRanges();
@@ -46,6 +59,46 @@ const initWhiskeyByBrand = whiskey => {
   state.whiskeyByBrand = groupBy(whiskey, 'Brand');
 };
 
+const initBrandsByCountry = whiskey => {
+  state.brandsByCountry = groupByWithSelect(whiskey, 'Country', 'Brand');
+};
+
+const initBudgetsByBrand = whiskey => {
+  const budgetsByBrand = {};
+  const whiskeyByBrand = groupBy(whiskey, 'Brand');
+  for (const brand of Object.keys(whiskeyByBrand)) {
+    const set = new Set();
+    for (const whiskey of whiskeyByBrand[brand]) {
+      const price = parseFloat(whiskey.Price.slice(1));
+      if (price >= 0 && price <= 30) {
+        set.add(budgetRanges[0]);
+      } else if (price >= 30 && price <= 60) {
+        set.add(budgetRanges[1]);
+      } else if (price >= 60 && price <= 90) {
+        set.add(budgetRanges[2]);
+      } else if (price >= 90 && price <= 120) {
+        set.add(budgetRanges[3]);
+      } else if (price >= 120 && price <= 150) {
+        set.add(budgetRanges[4]);
+      } else if (price >= 150 && price <= 2000) {
+        set.add(budgetRanges[5]);
+      }
+    }
+
+    const budgets = [];
+    for (let i = 0; i < budgetRanges.length; i++) {
+      if (set.has(budgetRanges[i])) {
+        set.delete(budgetRanges[i]);
+        budgets.push(budgetRanges[i]);
+      }
+    }
+
+    budgetsByBrand[brand] = budgets;
+  }
+
+  state.budgetsByBrand = budgetsByBrand;
+};
+
 const initCountries = whiskey => {
   const groupedByCountryWhiskey = groupBy(whiskey, 'Country');
   const countries = Object.keys(groupedByCountryWhiskey).sort((a, b) =>
@@ -55,14 +108,7 @@ const initCountries = whiskey => {
 };
 
 const initBudgetRanges = () => {
-  state.budgetRanges = [
-    '$0 - $30',
-    '$30 - $60',
-    '$60 - $90',
-    '$90 - $120',
-    '$120 - $150',
-    '$150 - $2000',
-  ];
+  state.budgetRanges = budgetRanges;
 };
 
 const initBrands = whiskey => {
@@ -111,4 +157,6 @@ export const getWhiskeyByBrand = () => state.whiskeyByBrand;
 export const getMainCategories = () => state.mainCategories;
 export const getCountries = () => state.countries;
 export const getBrands = () => state.brands;
+export const getBrandsByCountry = country => state.brandsByCountry[country];
+export const getBudgetsByBrand = brand => state.budgetsByBrand[brand];
 export const getBudgetRanges = () => state.budgetRanges;
