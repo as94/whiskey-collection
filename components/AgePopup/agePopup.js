c432f841200df266aef0adf5cc3a3ab9ea252053
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2023-10-17 22:42:07 +0700

    add styles for 1600px

--- components/AgePopup/agePopup.js
+++ components/AgePopup/agePopup.js
@@ -7,14 +7,16 @@ const component = `
 <link rel="stylesheet" href="./components/AgePopup/agePopup.css" />
 <div class="age-popup-background" id="age-popup-background">
     <div class="age-popup">
-        <h1 class="h1">Whiskey Collection</h1>
-        <h2 class="h2">Welcome! You must be +18 to enter</h2>
-        <h3 class="h3">Are you 18 years old or older?</h3>
+      <div class="age-popup-content">
+        <h1 class="h1 title">Whiskey Collection</h1>
+        <h2 class="h2 disclaimer">Welcome! You must be +18 to enter</h2>
+        <h3 class="h3 question">Are you 18 years old or older?</h3>
         <div class="answers">
             <button class="yes-btn body-semibold" data-no-select>Yes</button>
             <button class="no-btn body-semibold" data-no-select>No</button>
         </div>
         <img src="./components/AgePopup/logo-light-text.svg" title="Whiskey collection logo" alt="Glass of whiskey" />
+      </div>
     </div>
 </div>
 `;
