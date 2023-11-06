3753bcabec922fe5b32c96846f54bb17845277a7
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2023-11-06 17:53:45 +0700

    add whiskey collection club page add sending notification to telegram bot

---
+++ components/WhiskeyCollectionClubContent/whiskeyCollectionClubContent.js
@@ -0,0 +1,52 @@
+import { sendNotification } from '../../services/sendNotification.js';
+
+const whiskeyCollectionClubContent = `
+<link rel="stylesheet" href="./components/WhiskeyCollectionClubContent/whiskeyCollectionClubContent.css" />
+<div class="whiskey-collection-club-content" data-no-select>
+    <div class="whiskey-collection-club-offer">
+        <h1 class="h1 title">WHISKEY-COLLECTION CLUB</h1>
+        <p class="welcome-text body-medium-large">Welcome to The Whiskey Collection, where whiskey passion, friendliness and intimate knowledge of the spirit come together. Our club is dedicated to celebrating the great art of whiskey making and its rich variety of flavours. It doesn't matter if you are an experienced connoisseur or just starting your journey in the world of whiskey, our club is open to everyone who appreciates this amazing drink.</p>
+
+        <p class="call-to-action-text body-semibold">If you want to join us, just enter your email and we will contact you</p>
+        <div class="call-to-action-block">
+            <input id="email" class="email" type="text" placeholder="Enter your email" />
+            <button class="join-club-btn body-semibold" data-no-select>I want to join your club</button>
+        </div>
+
+        <p id="error" class="error"></p>
+    </div>
+    <img src="./components/WhiskeyCollectionClubContent/whiskeybottle.svg" title="Join our Whiskey-Collection club" alt="Bottle of whiskey" />
+</div>`;
+
+const element = document.getElementById('whiskeyCollectionClubContent');
+if (element) {
+  element.innerHTML = whiskeyCollectionClubContent;
+}
+
+async function handleClick() {
+  const email = document.querySelector('#email').value;
+  const error = document.querySelector('#error');
+
+  if (!email || !email.includes('@')) {
+    error.style.display = 'block';
+    error.textContent =
+      'Email is not correct. Please, check that the field is email and not empty';
+    return;
+  } else {
+    error.style.display = 'none';
+  }
+
+  const succeed = await sendNotification(email);
+  if (!succeed) {
+    error.style.display = 'block';
+    error.textContent =
+      'Something goes wrong with sending email. Please, try later. You also can contact administrator whiskeycollections@gmail.com';
+    return;
+  }
+
+  // redirect to success page
+}
+
+document.querySelector('.join-club-btn').addEventListener('click', function () {
+  handleClick();
+});
