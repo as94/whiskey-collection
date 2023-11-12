2911913ee982071bab3f510554e3cad0f8820ebf
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2023-11-12 14:30:39 +0700

    extract agePopup html

--- components/AgePopup/agePopup.js
+++ components/AgePopup/agePopup.js
@@ -3,28 +3,13 @@ import {
   setConfirmUsersAgeCookie,
 } from '../../services/cookieStorage.js';
 
-const component = `
-<link rel="stylesheet" href="./components/AgePopup/agePopup.css" />
-<div class="age-popup-background" id="age-popup-background">
-    <div class="age-popup">
-      <div class="age-popup-content">
-        <h1 class="h1 title">Whiskey Collection</h1>
-        <h2 class="h2 disclaimer">Welcome! You must be +18 to enter</h2>
-        <h3 class="h3 question">Are you 18 years old or older?</h3>
-        <div class="answers">
-            <button class="yes-btn body-semibold" data-no-select>Yes</button>
-            <button class="no-btn body-semibold" data-no-select>No</button>
-        </div>
-        <img src="./components/AgePopup/logo-light-text.svg" title="Whiskey collection logo" alt="Glass of whiskey" />
-      </div>
-    </div>
-</div>
-`;
+const response = await fetch('./components/AgePopup/agePopup.html');
+const htmlContent = await response.text();
 
 const agePopup = () => {
   const userConfirmedAgeCookie = getUserConfirmedAgeCookie();
   if (userConfirmedAgeCookie === null || !JSON.parse(userConfirmedAgeCookie)) {
-    return component;
+    return htmlContent;
   } else {
     return '';
   }
