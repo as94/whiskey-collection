19d04b7626b5d64eeff42500a4680bca7b74b462
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2023-11-12 15:55:31 +0700

    extract category description to html

--- components/CategoryDescription/categoryDescription.js
+++ components/CategoryDescription/categoryDescription.js
@@ -1,20 +1,23 @@
 import { getCategory } from '../../services/urlSearchParams.js';
 import { whiskeyCategoryDescriptions } from '../../services/whiskeyCategoryDescriptions.js';
 
+const response = await fetch(
+  './components/CategoryDescription/categoryDescription.html'
+);
+const htmlContent = await response.text();
+
 const categoryDescription = () => {
   const category = getCategory();
-  return `
-<link rel="stylesheet" href="./components/CategoryDescription/categoryDescription.css" />
-<div class="category-description">
-  <img class="category-image" src="${whiskeyCategoryDescriptions[category].ImageLink}" title="${category}" alt="${category}" />
-  <div class="description">
-    <h2 class="title h2">${category}</h2>
-    <p class="text body-medium-large">
-      ${whiskeyCategoryDescriptions[category].Description}
-    </p>
-  </div>
-</div>
-`;
+
+  return htmlContent
+    .replace('${imageLink}', whiskeyCategoryDescriptions[category].ImageLink)
+    .replace('${category}', category)
+    .replace('${category}', category)
+    .replace('${category}', category)
+    .replace(
+      '${description}',
+      whiskeyCategoryDescriptions[category].Description
+    );
 };
 
 const element = document.getElementById('category-description');
