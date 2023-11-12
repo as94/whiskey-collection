60efc015cb8c015c50be4fcf51ffdd8156e0640f
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2023-11-12 18:45:25 +0700

    extract world whiskey catalog to html

--- components/WorldWhiskeyCatalog/worldWhiskeyCatalog.js
+++ components/WorldWhiskeyCatalog/worldWhiskeyCatalog.js
@@ -9,6 +9,16 @@ import { whiskeyCategoryDescriptions } from '../../services/whiskeyCategoryDescr
 
 await initializeWhiskey();
 
+const response = await fetch(
+  './components/WorldWhiskeyCatalog/worldWhiskeyCatalog.html'
+);
+const htmlContent = await response.text();
+
+const cardResponse = await fetch(
+  './components/WorldWhiskeyCatalog/worldWhiskeyCard.html'
+);
+const cardHtmlContent = await cardResponse.text();
+
 const getCatalogItems = () => {
   let result = '';
   const categories = getMainCategories();
@@ -17,48 +27,35 @@ const getCatalogItems = () => {
     const category = categories[index];
     const whiskey = getRandomItem(whiskeyByCategory[category]);
 
-    result += `<a class="card" data-whiskey-category="${category}" data-no-select href="${getCatalogByCategoriesLink(
-      category
-    )}">
-        <h3 class="category-name h3">${category}</h3>
-        <div class="category-link-container">
-        <div class="category-link body-semibold"">
-            Discover more
-        </div>
-        </div>
-        <div class="category-image">
-        <img
-            class="background-image"
-            src="${
-              index % 2 === 0
-                ? 'images/product-card-backgrounds/dark-green.webp'
-                : 'images/product-card-backgrounds/light-green.webp'
-            }"
-        />
-        <img class="foreground-image" src="${
-          whiskey.ImageLink
-        }" title="${category}" alt="${category}" />
-        </div>
-        <p class="category-description body-semibold">
-          ${
-            whiskeyCategoryDescriptions[category].Description.split(/\s+/)
-              .map(x => x.replace(',', ''))
-              .slice(0, 15)
-              .join(' ') + ' ...'
-          }
-        </p>
-    </a>`;
+    result += cardHtmlContent
+      .replace('${category}', category)
+      .replace(
+        '${catalogByCategoriesLink}',
+        getCatalogByCategoriesLink(category)
+      )
+      .replace('${category}', category)
+      .replace(
+        '${categoryBackground}',
+        index % 2 === 0
+          ? 'images/product-card-backgrounds/dark-green.webp'
+          : 'images/product-card-backgrounds/light-green.webp'
+      )
+      .replace('${whiskeyImageLink}', whiskey.ImageLink)
+      .replace('${category}', category)
+      .replace('${category}', category)
+      .replace(
+        '${description}',
+        whiskeyCategoryDescriptions[category].Description.split(/\s+/)
+          .map(x => x.replace(',', ''))
+          .slice(0, 15)
+          .join(' ') + ' ...'
+      );
   }
 
   return result;
 };
 
-const catalog = () => `
-<link rel="stylesheet" href="./components/WorldWhiskeyCatalog/worldWhiskeyCatalog.css" />
-<div class="catalog">
-  ${getCatalogItems()}
-</div>
-`;
+const catalog = () => htmlContent.replace('${catalogItems}', getCatalogItems());
 
 const element = document.getElementById('world-whiskey-catalog');
 if (element) {
