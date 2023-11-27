255565d8c2b3266633fe3d322236861d375c4c63
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2023-11-27 15:01:04 +0700

    finished post

--- src/components/Blog/Post/post.js
+++ src/components/Blog/Post/post.js
@@ -9,37 +9,55 @@ if (element) {
   const postTitleKey = getPostTitleKey();
   const post = getPost(postTitleKey);
 
-  // const content = post.article;
-
-  // const title = document.createElement('h1');
-  // title.textContent = content.title;
-  // element.appendChild(title);
-
-  // const tags = document.createElement('p');
-  // tags.textContent = content.tags.join(', '); // TODO: for a while
-  // element.appendChild(tags);
-
-  // const previewImg = document.createElement('img');
-  // previewImg.src = `assets/posts/${content.mainImagePath}`;
-  // previewImg.alt = content.mainImagePath
-  //   .replace(/^.*[\\\/]/, '')
-  //   .replace(/\.[^/.]+$/, '');
-  // element.appendChild(previewImg);
-
   var tempContainer = document.createElement('div');
 
   const converter = new showdown.Converter();
   tempContainer.innerHTML = converter.makeHtml(post.markdownContent);
 
-  const imgElements = tempContainer.querySelectorAll('img');
-  for (let i = imgElements.length - 1; i >= 0; i--) {
-    if (i % 2 === 0) {
-      imgElements[i].style.float = 'left';
-      imgElements[i].style.marginRight = '15px';
+  const h2Elements = tempContainer.querySelectorAll('h2');
+  for (let i = 0; i < h2Elements.length; i++) {
+    h2Elements[i].classList.add('h2');
+  }
+
+  const h3Elements = tempContainer.querySelectorAll('h3');
+  for (let i = 0; i < h3Elements.length; i++) {
+    h3Elements[i].classList.add('h3');
+  }
+
+  const pElements = tempContainer.querySelectorAll('p');
+  for (let i = 0; i < pElements.length; i++) {
+    if (i === 0) {
+      pElements[i].classList.add('post-preview');
     } else {
-      imgElements[i].style.float = 'right';
-      imgElements[i].style.marginLeft = '15px';
+      pElements[i].classList.add('post-paragraph');
     }
+  }
+
+  const imgElements = tempContainer.querySelectorAll('img');
+  for (let i = 0; i < imgElements.length; i++) {
+    imgElements[i].style.width = '400px';
+    imgElements[i].style.marginTop = '25px';
+    imgElements[i].style.marginBottom = '25px';
+    if (i % 2 === 0) {
+      imgElements[i].style.float = 'right';
+      imgElements[i].style.marginLeft = '25px';
+    } else {
+      imgElements[i].style.float = 'left';
+      imgElements[i].style.marginRight = '25px';
+    }
+  }
+
+  const aElements = tempContainer.querySelectorAll('a');
+  for (let i = 0; i < aElements.length; i++) {
+    const productName = aElements[i].textContent;
+    const link = `catalog-by-search-results?searchText=${encodeURIComponent(
+      productName
+    )}&page=1`;
+
+    aElements[i].href = link;
+
+    aElements[i].target = '_blank';
+    aElements[i].rel = 'noopener noreferrer';
   }
 
   const tags = post.article.tags.map(
@@ -53,7 +71,7 @@ if (element) {
     .replace('${readingTimeInMinutes}', post.article.readingTimeInMinutes)
     .replace(
       '${publicationDate}',
-      new Date(post.article.publicationDate).toLocaleDateString()
+      new Date(post.article.publicationDate).toDateString()
     )
     .replace('${articleContent}', tempContainer.innerHTML);
 }
