6a470cdc0d1a056af2cc73bd60591a1d56f9b7fc
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2024-02-01 09:21:14 +0700

    use simple signInWithPopup method remove loader

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,38 +3,28 @@ import './header.css';
 
 import { getWithExpiry } from '../../services/localStorage';
 
-import {
-  googleSignIn,
-  signOut,
-  handleSignInResult,
-} from '../../services/firebase.js';
+import { googleSignIn, signOut } from '../../services/firebase.js';
 
 const element = document.getElementById('mainHeader');
 if (element) {
   element.innerHTML = headerContent;
-  // const loadingWrapper = document.querySelector('.loading-wrapper');
-  // loadingWrapper.style.display = 'flex';
 
-  // // await handleSignInResult();
+  const isAuthenticated = getWithExpiry('userName');
 
-  // loadingWrapper.style.display = 'none';
+  const signInWrapper = document.querySelector('.sign-in-wrapper');
+  const signOutWrapper = document.querySelector('.sign-out-wrapper');
 
-  // const isAuthenticated = getWithExpiry('userName');
+  if (isAuthenticated) {
+    signOutWrapper.style.display = 'flex';
+  } else {
+    signInWrapper.style.display = 'flex';
+  }
 
-  // const signInWrapper = document.querySelector('.sign-in-wrapper');
-  // const signOutWrapper = document.querySelector('.sign-out-wrapper');
+  signInWrapper.addEventListener('click', function () {
+    googleSignIn();
+  });
 
-  // if (isAuthenticated) {
-  //   signOutWrapper.style.display = 'flex';
-  // } else {
-  //   signInWrapper.style.display = 'flex';
-  // }
-
-  // signInWrapper.addEventListener('click', function () {
-  //   googleSignIn();
-  // });
-
-  // signOutWrapper.addEventListener('click', function () {
-  //   signOut();
-  // });
+  signOutWrapper.addEventListener('click', function () {
+    signOut();
+  });
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
-  signInWithRedirect,
   signInWithPopup,
-  getRedirectResult,
   GoogleAuthProvider,
   signOut as signOutFirebase,
 } from 'firebase/auth';
-import {
-  setWithExpiry,
-  getWithExpiry,
-  twoWeeksExpiration,
-  remove,
-} from './localStorage';
+import { setWithExpiry, twoWeeksExpiration } from './localStorage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAMb_3E4JAzBpXXXZs-K64XcRDE_rySA-Q',
@@ -29,41 +22,11 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 auth.languageCode = auth.useDeviceLanguage();
 
-// auth.onAuthStateChanged(user => {
-//   const isAuthenticated = getWithExpiry('userName');
-//   if (isAuthenticated) {
-//     return;
-//   }
-
-//   getRedirectResult(auth)
-//     .then(result => {
-//       if (result) {
-//         const credential = GoogleAuthProvider.credentialFromResult(result);
-
-//         setWithExpiry('token', credential.accessToken, twoWeeksExpiration);
-//         setWithExpiry('userName', result.user.displayName, twoWeeksExpiration);
-//         setWithExpiry('userEmail', result.user.email, twoWeeksExpiration);
-//       } else {
-//         setWithExpiry('userName', user.displayName, twoWeeksExpiration);
-//         setWithExpiry('userEmail', user.email, twoWeeksExpiration);
-//       }
-//     })
-//     .catch(error => {
-//       console.error('Error authenticating user:', error);
-//     })
-//     .finally(() => {
-//       remove('loginInProgress');
-//     });
-// });
-
 const provider = new GoogleAuthProvider();
 provider.addScope('https://www.googleapis.com/auth/userinfo.email');
 provider.addScope('https://www.googleapis.com/auth/userinfo.profile');
 
 export const googleSignIn = async () => {
-  // setWithExpiry('loginInProgress', 'true', twoWeeksExpiration);
-  // signInWithRedirect(auth, provider);
-
   const result = await signInWithPopup(auth, provider);
 
   const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -71,6 +34,8 @@ export const googleSignIn = async () => {
   setWithExpiry('token', credential.accessToken, twoWeeksExpiration);
   setWithExpiry('userName', result.user.displayName, twoWeeksExpiration);
   setWithExpiry('userEmail', result.user.email, twoWeeksExpiration);
+
+  location.reload();
 };
 
 export const signOut = () => {
@@ -78,5 +43,3 @@ export const signOut = () => {
   window.localStorage.clear();
   location.reload();
 };
-
-export const handleSignInResult = async () => {};
